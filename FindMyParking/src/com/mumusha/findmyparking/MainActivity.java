d5b6f72480a0ac1a40cbad0e55050d7b9fd82e9a
gaomy007 <gaomy1202@gmail.com>
2014-04-03 01:09:22 -0700

    new activity

--- FindMyParking/src/com/mumusha/findmyparking/MainActivity.java
+++ FindMyParking/src/com/mumusha/findmyparking/MainActivity.java
@@ -1,24 +1,43 @@
 package com.mumusha.findmyparking;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+//import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	public static final String ACTION_NEW = "org.xmlvm.tutorial.intent.action.ACTION_NEW";
+    //private TextView           resultField;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		int t=0;
-		if(t!=0);
 		return true;
 	}
+	
+	 /**
+     * This method will be invoked whenever the button is clicked in the
+     * BlueActivity. This will trigger the invocation of YellowActivity.
+     */
+    public void onClick(View v) {
+        //resultField.setText("");
+        /**
+         * Construct an Intent to launch NewRecord. Note that
+         * NewRecord is referenced explicitly via its class object.
+         */
+        Intent intent = new Intent(this, NewRecordActivity.class);
+        intent.setClass(MainActivity.this, NewRecordActivity.class);
+        startActivity(intent);
+        
+    }
 
 }
---
+++ FindMyParking/src/com/mumusha/findmyparking/NewRecordActivity.java
@@ -0,0 +1,12 @@
+package com.mumusha.findmyparking;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class NewRecordActivity {
+
+}
