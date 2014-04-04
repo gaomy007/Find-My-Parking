05e37e0e392760a95440af99d657d8a0c829a4b0
gaomy007 <gaomy1202@gmail.com>
2014-04-03 21:01:28 -0700

    linked and rearranged the layout

--- FindMyParking/gen/com/mumusha/findmyparking/R.java
+++ FindMyParking/gen/com/mumusha/findmyparking/R.java
@@ -22,20 +22,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_launcher1=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
-        public static final int button1=0x7f080003;
-        public static final int button2=0x7f080004;
-        public static final int editText1=0x7f08000a;
-        public static final int imageButton1=0x7f080008;
-        public static final int imageView1=0x7f080009;
-        public static final int maptext=0x7f080000;
-        public static final int notetext=0x7f080002;
-        public static final int phototext=0x7f080001;
-        public static final int textView1=0x7f080005;
-        public static final int textureView1=0x7f080006;
-        public static final int timePicker1=0x7f080007;
+        public static final int action_settings=0x7f08000c;
+        public static final int button1=0x7f080007;
+        public static final int button2=0x7f080001;
+        public static final int buttonOk=0x7f08000b;
+        public static final int buttonReset=0x7f08000a;
+        public static final int editText1=0x7f080009;
+        public static final int imageButton1=0x7f080004;
+        public static final int imageView1=0x7f080005;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080000;
+        public static final int textViewUserInput=0x7f080006;
+        public static final int textureView1=0x7f080003;
+        public static final int timePicker1=0x7f080008;
     }
     public static final class layout {
         public static final int activity_find_record=0x7f030000;
--- FindMyParking/src/com/mumusha/findmyparking/NewRecord.java
+++ FindMyParking/src/com/mumusha/findmyparking/NewRecord.java
@@ -1,7 +1,11 @@
 package com.mumusha.findmyparking;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class NewRecord extends Activity {
 
@@ -10,6 +14,34 @@ public class NewRecord extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_record);
 	}
+	
+	
+    public void onClickReset(View v) {
+        //resultField.setText("");
+        /**
+         * Construct an Intent to launch NewRecord. Note that
+         * NewRecord is referenced explicitly via its class object.
+         */
+    	EditText NoteText = (EditText) findViewById(R.id.editText1);  
+    	NoteText.setText(null);
+    	
+    	TimePicker tmp = (TimePicker) this.findViewById(R.id.timePicker1);
+        tmp.setCurrentHour(0);
+        tmp.setCurrentMinute(0);
+    }
+    
+    
+    public void onClickOk(View v) {
+        //resultField.setText("");
+        /**
+         * Construct an Intent to launch NewRecord. Note that
+         * NewRecord is referenced explicitly via its class object.
+         */
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setClass(NewRecord.this,MainActivity.class);
+        startActivity(intent);
+        
+    }
 
 	public void onBackPressed() {
 
